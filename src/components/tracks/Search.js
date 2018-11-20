75b986aa2adeca21294f2aadf9e841dfb2388540
Fred <fgravil18@gmail.com>
2018-11-20 01:09:20 -0500

    update to support artist search.

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -2,26 +2,42 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import {Consumer} from '../../context';
 
+const styles = {
+    dropdownItem: {
+        cursor: 'pointer'
+    }
+}
 export default class Search extends Component {
+    searchByOptions = ['Artist', 'Song'];
     state = {
-        trackTitle: ''
+        searchBy: 'Song',
+        trackInfo: ''
     }
 
     findTrack = (dispatch, e) => {
+        let {searchBy, trackInfo} = this.state;
+        searchBy = searchBy === 'Song' ? 'track' : searchBy.toLowerCase();
         e.preventDefault();
-        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/${searchBy}.search?q_${searchBy}=${trackInfo}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
         .then(res => {
             dispatch({
                 type: 'SEARCH_TRACKS',
                 payload: res.data.message.body.track_list
             });
-            this.setState({trackTitle: ''});
+            this.setState({...this.state, trackInfo: ''});
         })
         .catch(err => console.log(err));
     }
+    
     onChange = e => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({trackInfo: e.target.value});
     }
+
+    updateSearchBy = (searchBy) => {
+        console.log(searchBy);
+        this.setState({...this.state, searchBy: searchBy})
+    }
+
     render() {
         return (
             <Consumer>
@@ -34,14 +50,36 @@ export default class Search extends Component {
                             <p className="lead text-center">Get the lyrics for any song</p>
                             <form onSubmit={this.findTrack.bind(this, value.dispatch)}>
                                 <div className="form-group">
-                                    <input 
-                                        type="text" 
-                                        className="form-control form-control-lg" 
-                                        placeholder="Song title..."
-                                        name="trackTitle"
-                                        value={this.state.trackTitle} 
-                                        onChange={this.onChange}
-                                    />
+                                    <div className="input-group">
+                                        <div class="input-group-prepend">
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                {this.state.searchBy}
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                {
+                                                    this.searchByOptions.map((value, index) => 
+                                                        <li 
+                                                            key={index} 
+                                                            className="dropdown-item" 
+                                                            style={styles.dropdownItem}
+                                                            onClick={() => this.updateSearchBy(value)}>
+                                                            {value}
+                                                        </li>
+                                                    )
+                                                }
+                                            </div>
+                                            </div>
+                                        </div>
+                                        <input 
+                                            type="text" 
+                                            className="form-control form-control-lg" 
+                                            placeholder={`${this.state.searchBy} name...`}
+                                            name="title"
+                                            value={this.state.trackInfo} 
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
                                 </div>
                                 <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">Get Track Lyrics</button>
                             </form>
